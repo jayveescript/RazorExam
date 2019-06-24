82b93eea4a5c9fe3dbc026272fc3606200a6b359
Jayvee <jayveev@realtair.com>
2019-06-24 18:29:14 +0800

    Add input number then submit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var homeController = new HomeController();
+            var result = homeController.Index() as ViewResult;
+            Assert.IsInstanceOfType(result.Model, typeof(TestModel));
+        }
+    }
+}
